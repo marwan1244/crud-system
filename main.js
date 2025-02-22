bc6e8682cfa435fe44af05407541ec7ffdc56d1c
Marwan Ashraf <ma7109677@gmail.com>
2025-02-22 14:03:04 +0200

    Add files via upload

---
+++ main.js
@@ -0,0 +1,150 @@
+// console.log("hello world!");
+
+// var myname = "marwan";
+// const pi = 3.14;
+// let name = "dssd";
+// var a = 5;
+// var b = 2;
+// var c = "i am a";
+
+// a = a + 1;
+// b = b + 2;
+// c = c + " string";
+
+// console.log(a);
+// console.log(b);
+// console.log(c);
+
+// var w = 4*23145;
+// console.log(w);
+
+// var str = "asas\\asasa\asas\sa\s";
+// console.log(str);
+
+// var s = "asasa" + "asccc";
+
+// console.log(s);
+
+// var first = "marwan";
+
+// var second = first[first.length / 2];
+
+// console.log(second);
+
+
+// var arr = [[1,2],[3,4]];
+
+// console.log(arr[1]);
+
+
+// var myarr = [[1,2,3],[4,5,6],[7,8,9],[[10,11,12]]];
+
+// console.log(myarr[0][1]);
+
+
+// var myarr2 = ["marwan", "sayed","adsa"];
+// myarr2.push(["qw","sdsfd"]);
+// myarr2.pop();
+// myarr2.shift();
+// myarr2.unshift("ragab");
+// console.log(myarr2); 
+
+
+
+// var shopping = [['orange',4] , ['apple' ,2] , ['banans' , 7],['eggs' ,12]];
+
+// shopping.push(['cake' , 3]);
+// shopping.shift();
+// console.log(shopping);
+
+
+// let myglobal = 12;
+
+// function fun2()
+// {
+//     let privte = 15;
+// }
+
+
+// function fun1()
+// {
+//     let outbut = "";
+//     if(typeof myglobal != "undefined")
+//         {
+//             outbut += myglobal;
+//         }
+//         if(typeof privte !="undefined")
+//             {
+//                 outbut += privte;
+//             }
+//         console.log(outbut);
+//         console.log(privte);
+// }
+
+// fun1();
+
+// fun2();
+
+
+// var changed = 0;
+
+// function change(num) {
+//     return (num + 5) / 3;
+// }
+// changed = change(10);
+
+// console.log(changed); // Corrected: Logs the result, not the function
+
+// var processed = 0;
+
+// function process(num) {
+//     return (num + 3) / 5;
+// }
+// processed = process(17);
+
+// console.log(processed); // Corrected: Logs the result, not the function
+
+
+
+// var person = {
+//     firstName: "John",
+//     lastName: "Doe",
+//     age: 50,
+//     eyeColor: "blue"
+//   };
+//   person.address = "cairo";
+//   person.firstName = "marwan";
+//   delete person.age;
+// console.log(person);
+
+
+// var man =
+// {
+//     name:'marwan',
+//     age:22,
+//     address:"cairo",
+//     sister:
+//     {
+//         name:'omnia',
+//         age:28,        
+//     }
+// };
+// console.log(man);
+// man.sister.job = 'teacher';
+
+
+// function convertoint(str)
+// {
+//     return parseInt(str);
+// }
+
+//  console.log( typeof(convertoint("122432")))
+
+
+//  function convertoint(str)
+//  {
+//      return parseInt(str, 2);
+//  }
+//  console.log(
+//     convertoint("10011")
+//  )
